e70b59f7ed015a77c654fc75c3d6c658f586e189
hitch909 <evgenij909@yandex.ru>
2023-01-19 14:51:20 +0500

    задачка № 13 готова

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,3 +1,13 @@
 ﻿// программу, которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет.
 
+Console.WriteLine("введите любое целое число.");
+int number = int.Parse(Console.ReadLine()!);
+int result = number / 100;
+int and_result = result % 10;
+if (result <=0) Console.WriteLine("данное число третьей цифры не имеет.");
+else
+{
+Console.WriteLine($"цифра- {and_result} является третьей цифрой числа {number}. ");
+}
+//  готово.
