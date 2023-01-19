4222d851812d333ef68d522713fb3d8a11b42f85
hitch909 <evgenij909@yandex.ru>
2023-01-19 14:27:01 +0500

    задача №10.

---
+++ Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("введите любое целое трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int result = number % 100;
+int and_result = result / 10;
+Console.WriteLine($"{and_result} -вторая цифра от введенного числа.");
+//  готово.
